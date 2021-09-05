b34637c8502fec631caa449a1f19976071393123
Siddharth <sidbhasin82@gmail.com>
2021-09-05 12:44:37 +0530

    Adding commit in two knights solution

--- introductory-problems/two-knights.cpp
+++ introductory-problems/two-knights.cpp
@@ -24,7 +24,7 @@ int main() {
 			total_ways_to_place_two_pieces = (k_sqr_minus_1/2)*k_sqr;
 
 		ll num_of_3_x_2_rects_on_board = 2LL * (k-1) * (k-2); //2 constant is for 3x2 and 2x3
-		ll total_ways_knights_attack_on_board = num_of_3_x_2_rects_on_board * 2;
+		ll total_ways_knights_attack_on_board = num_of_3_x_2_rects_on_board * 2; //2 ways to places 2 knights attacking each other in 3x2 rect
 
 		cout << (total_ways_to_place_two_pieces - total_ways_knights_attack_on_board) << "\n";
 
